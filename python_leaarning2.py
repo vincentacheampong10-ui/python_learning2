fff48c8639dd696a63bc284f1ec1b0d40bf5a736
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-01-28 07:03:24 -0500

    2D Numpad

--- python_leaarning2.py
+++ python_leaarning2.py
@@ -1,12 +1,24 @@
-# 2D Collections
+# 2D Numpad
 
-fruits = ["apples", "banana", "coconut"]
-vegetable = ["celery", "carrots", "potatoes"]
-meats = ["chicken","fish","turkey"]
+# fruits = ["apples", "banana", "coconut"]
+# vegetable = ["celery", "carrots", "potatoes"]
+# meats = ["chicken","fish","turkey"]
+#
+# groceries = [fruits, vegetable, meats]
+#
+# for collection in groceries:
+#     for food in collection:
+#       print(food, end= " ")
+#     print()
 
-groceries = [fruits, vegetable, meats]
+numpad_1 = [1, 2, 3]
+numpad_2 = [4, 5, 6]
+numpad_3 = [7, 8, 9]
+numpad_4 = ["*", "0", "#"]
 
-for collection in groceries:
-    for food in collection:
-      print(food, end= " ")
+calculator = [numpad_1, numpad_2, numpad_3, numpad_4]
+
+for num in calculator:
+    for row in num:
+     print(row,  end=" ")
     print()
